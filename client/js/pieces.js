9078dbe21703ba5943f820a2ab4a7541b59b46a3
Gavin Shriver <gavin.shriver@gmail.com>
2013-12-16 23:53:28 -0800

    builds board and attempts to use angular

--- client/js/pieces.js
+++ client/js/pieces.js
@@ -1,12 +1,17 @@
 // the prototype of all pieces:
-var Piece = function(x,y){
+var Piece = function(x, y, color){
   this.x = x || 0;
   this.y = y || 0;
+  this.color = color || "black";
   this.movesDiagonally = true;
   this.movesForwardAndBack = true;
   this.movesLeftAndRight = true;
   this.movementLimit = 7; //two spaces
 };
+
+Piece.prototype.getIMG = function(){
+  return this.img_src[this.color];
+}
 
 Piece.prototype.setPos = function(x,y){
   this.x = x || this.x;
@@ -27,12 +32,12 @@ Piece.prototype.moveTo = function(x,y){
 };
 
 Piece.prototype.isValidMove = function(x,y){
-  var okayMoves = canMoveTo();
-  if(okayMoves[y][x]){ return true;}
+  var okayMoves = this.canMoveTo();
+  if(okayMoves[y][x]){return true;}
   return false;
 };
 
-Piece.prototype._makeBlankBoard = function(){
+Piece.makeBlankBoard = function(){
   var board = [];
   for (var i = 0; i < 8; i++) {
     board.push([]);
@@ -43,28 +48,29 @@ Piece.prototype._makeBlankBoard = function(){
   return board;
 }
 
-Piece.prototype.canMoveTo = function(board){
-  var board = this._makeBlankBoard();
+Piece.prototype.canMoveTo = function(){
+  var board = Piece.makeBlankBoard();
 
   for (var i = -this.movementLimit; i <= this.movementLimit; i++) {
+
     // foward and backward movements
     // first check that the square is on the board
-    if(this.movesFowardAndBack && this.movesboard[this.y+i][this.x] === false){ 
+    if(this.movesForwardAndBack && this.y+i >= 0 && this.y+i <= 7){ 
       board[this.y+i][this.x] = true;
     }
     // right and left movements
     // first check that the square is on the board
-    if(this.movesLeftAndRight && board[this.y][this.x+i] === false){ 
+    if(this.movesLeftAndRight && this.x+i >= 0 && this.x+i <= 7){ 
       board[this.y][this.x+i] = true;
     }
     // diagonal movements
     // first check that the square is on the board
-    if(this.movesDiagonally && board[this.y+i][this.x+i] === false){ 
+    if(this.movesDiagonally && this.y+i >= 0 && this.y+i <= 7 && this.x+i >= 0 && this.x+i <= 7){ 
       board[this.y+i][this.x+i] = true;
     }
     // diagonal movements
     // first check that the square is on the board
-    if(this.movesDiagonally && board[this.y-i][this.x+i] === false){ 
+    if(this.movesDiagonally && this.y-i >= 0 && this.y-i <= 7 && this.x+i >= 0 && this.x+i <= 7){ 
       board[this.y-i][this.x+i] = true;
     }
   };
@@ -74,34 +80,34 @@ Piece.prototype.canMoveTo = function(board){
 
 
 
-
-
 //
 // PAWNS
 //
-var Pawn = new Piece();
-Pawn.prototype.constructor = function(x,y){
-  this.x = x;
-  this.y = y;
+var Pawn = function(x,y,color){
+  Piece.call(this,x,y,color); 
+
   this.movesDiagonally = false;
   this.movementLimit = 2;
   this.img_src = {
-    black: "images/pieces/Black_P.png",
-    blue: "images/pieces/Blue_P.png",
-    brown: "images/pieces/Brown_P.png",
-    clear: "images/pieces/Clear_white_P.png",
-    white: "images/pieces/White_P.png",
-    yellow: "images/pieces/Yellow_P.png",
+    'black': "images/pieces/Black_P.png",
+    'blue': "images/pieces/Blue_P.png",
+    'brown': "images/pieces/Brown_P.png",
+    'clear': "images/pieces/Clear_white_P.png",
+    'white': "images/pieces/White_P.png",
+    'yellow': "images/pieces/Yellow_P.png"
   };
 };
+Pawn.prototype = Object.create(Piece.prototype);
+Pawn.prototype.constructor = Pawn;
+
+
+
 
 //
 // PARALYZER
 //
-var Paralyzer = new Piece();
-Paralyzer.prototype.constructor = function(x,y){
-  this.x = x;
-  this.y = y;
+var Paralyzer = function(x,y, color){
+  Piece.call(this, x, y, color);
   this.movesDiagonally = false;
   this.img_src = {
     black: "images/pieces/Black_UR.png",
@@ -109,51 +115,53 @@ Paralyzer.prototype.constructor = function(x,y){
     brown: "images/pieces/Brown_UR.png",
     clear: "images/pieces/Clear_white_UR.png",
     white: "images/pieces/White_UR.png",
-    yellow: "images/pieces/Yellow_UR.png",
+    yellow: "images/pieces/Yellow_UR.png"
   };
-};
+}
+Paralyzer.prototype = Object.create(Piece.prototype);
+Paralyzer.prototype.constructor = Paralyzer;
+
 
 //
 // CHAMELEON
 //
-var Chameleon = new Piece();
-Chameleon.prototype.constructor = function(x,y){
-  this.x = x;
-  this.y = y;
+var Chameleon = function(x,y, color){
+  Piece.call(this, x, y, color);
   this.img_src = {
     black: "images/pieces/Black_B.png",
     blue: "images/pieces/Blue_B.png",
     brown: "images/pieces/Brown_B.png",
     clear: "images/pieces/Clear_white_B.png",
     white: "images/pieces/White_B.png",
-    yellow: "images/pieces/Yellow_B.png",
-  };
-};
+    yellow: "images/pieces/Yellow_B.png"
+  };  
+}
+Chameleon.prototype = Object.create(Piece.prototype);
+Chameleon.prototype.constructor = Chameleon;
+
 
 //
 // RETRACTOR
 //
-var Retractor = new Piece();
-Retractor.prototype.constructor = function(x,y){
-  this.x = x;
-  this.y = y;
+var Retractor = function(x, y, color){
+  Piece.call(this,x,y, color);
   this.img_src = {
     black: "images/pieces/Black_Q.png",
     blue: "images/pieces/Blue_Q.png",
     brown: "images/pieces/Brown_Q.png",
     clear: "images/pieces/Clear_white_Q.png",
     white: "images/pieces/White_Q.png",
-    yellow: "images/pieces/Yellow_Q.png",
+    yellow: "images/pieces/Yellow_Q.png"
   };
-};
+}
+Retractor.prototype = Object.create(Piece.prototype);
+Retractor.prototype.constructor = Retractor;
 
 //
 // KING
 //
-var King = new Piece();
-King.prototype.constructor = function(x,y){
-  this.x = x;
-  this.y = y;
+var King = function(x,y, color){
+  Piece.call(this, x, y, color);
   this.movementLimit = 1;
   this.img_src = {
     black: "images/pieces/Black_K.png",
@@ -161,24 +169,42 @@ King.prototype.constructor = function(x,y){
     brown: "images/pieces/Brown_K.png",
     clear: "images/pieces/Clear_white_K.png",
     white: "images/pieces/White_K.png",
-    yellow: "images/pieces/Yellow_K.png",
+    yellow: "images/pieces/Yellow_K.png"
   };
-};
+}
+King.prototype = Object.create(Piece.prototype);
+King.prototype.constructor = King;
 
 //
 // SYNCHRONIZER
 //
-var Synchronizer = new Piece();
-Synchronizer.prototype.constructor = function(x,y){
-  this.x = x;
-  this.y = y;
-  this.movementLimit = 1;
+var Synchronizer = function(x, y, color){
+  Piece.call(this, x, y, color);
   this.img_src = {
     black: "images/pieces/Black_R.png",
     blue: "images/pieces/Blue_R.png",
     brown: "images/pieces/Brown_R.png",
     clear: "images/pieces/Clear_white_R.png",
     white: "images/pieces/White_R.png",
-    yellow: "images/pieces/Yellow_R.png",
+    yellow: "images/pieces/Yellow_R.png"
   };
-};
+}
+Synchronizer.prototype = Object.create(Piece.prototype);
+Synchronizer.prototype.constructor = Synchronizer;
+
+//
+// JUMPER
+//
+var Jumper = function(x, y, color){
+  Piece.call(this, x, y, color);
+  this.img_src = {
+    black: "images/pieces/Black_N.png",
+    blue: "images/pieces/Blue_N.png",
+    brown: "images/pieces/Brown_N.png",
+    clear: "images/pieces/Clear_white_N.png",
+    white: "images/pieces/White_N.png",
+    yellow: "images/pieces/Yellow_N.png"
+  };
+}
+Jumper.prototype = Object.create(Piece.prototype);
+Jumper.prototype.constructor = Jumper;
--- tests/spec/piecesSpec.js
+++ tests/spec/piecesSpec.js
@@ -1,68 +1,477 @@
-beforeEach(function() {
-  this.addMatchers({
-    toBePlaying: function(expectedSong) {
-      var player = this.actual;
-      return player.currentlyPlayingSong === expectedSong && 
-             player.isPlaying;
-    }
-  });
-});
+describe("Pieces", function() {
 
-describe("Player", function() {
-  var player;
-  var song;
+  it("should have correct methods and variables", function(){
+    var piece = new Piece();
 
-  beforeEach(function() {
-    player = new Player();
-    song = new Song();
-  });
+    expect(Piece.makeBlankBoard).toEqual(jasmine.any(Function));
+    expect(piece.setPos).toEqual(jasmine.any(Function));
+    expect(piece.getPos).toEqual(jasmine.any(Function));
+    expect(piece.moveTo).toEqual(jasmine.any(Function));
+    expect(piece.isValidMove).toEqual(jasmine.any(Function));
+    expect(piece.canMoveTo).toEqual(jasmine.any(Function));
+    expect(piece.x).toEqual(jasmine.any(Number));
+    expect(piece.y).toEqual(jasmine.any(Number));
+    expect(piece.movementLimit).toEqual(jasmine.any(Number));
+    expect(typeof piece.movesDiagonally).toEqual('boolean');
+    expect(typeof piece.movesForwardAndBack).toEqual('boolean');
+    expect(typeof piece.movesLeftAndRight).toEqual('boolean');
 
-  it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
-
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
   });
 
-  describe("when song has been paused", function() {
+  it("setPos should set the position", function() {
+    var piece = new Piece();
+
+    piece.setPos(1,2);
+    expect(piece.x).toEqual(1);
+    expect(piece.y).toEqual(2);
+  });
+
+  it("getPos should return the position", function() {
+    var piece = new Piece();
+
+    var currentPos = piece.getPos();
+    expect(currentPos.x).toEqual(0);
+    expect(currentPos.y).toEqual(0);
+    piece.setPos(4,4);
+    var currentPos = piece.getPos();
+    expect(currentPos.x).toEqual(4);
+    expect(currentPos.y).toEqual(4);
+  });
+
+
+  //
+  //  PAWN TEST
+  //
+  describe("Pawns", function() {
     beforeEach(function() {
-      player.play(song);
-      player.pause();
+      var pawn = new Pawn(4,4);
     });
 
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
+    it("should have the correct properties", function() {
+      var pawn = new Pawn(4,4);
 
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
+      var currentPos = pawn.getPos();
+      expect(currentPos.x).toEqual(4);
+      expect(currentPos.y).toEqual(4);
+      expect(pawn.movesDiagonally).toBeFalsy();
+      expect(pawn.movementLimit).toEqual(2);
     });
 
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
+    it("canMoveTo should give a board with valid movements", function() {
+      var pawn = new Pawn(4,4);
+      
+      var currentPos = pawn.getPos();
+      var movePositions = pawn.canMoveTo();
+      expect(movePositions[currentPos.y][currentPos.x-2]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+2]).toBeTruthy();
+      expect(movePositions[currentPos.y-2][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+2][currentPos.x]).toBeTruthy();
+
+
+      expect(movePositions[currentPos.y+3][currentPos.x]).toBeFalsy();
+      expect(movePositions[currentPos.y-3][currentPos.x]).toBeFalsy();
+      expect(movePositions[currentPos.y][currentPos.x+3]).toBeFalsy();
+      expect(movePositions[currentPos.y][currentPos.x-3]).toBeFalsy();
+
+      expect(movePositions[currentPos.y+1][currentPos.x+1]).toBeFalsy();
+      expect(movePositions[currentPos.y-1][currentPos.x-1]).toBeFalsy();
+      expect(movePositions[currentPos.y-1][currentPos.x+1]).toBeFalsy();
+      expect(movePositions[currentPos.y+1][currentPos.x-1]).toBeFalsy();
+
     });
+
+    it("should be able to moveTo a valid location", function() {
+      var pawn = new Pawn(4,4);
+
+      var currentPos = pawn.getPos();
+      var moved = pawn.moveTo(4,3);
+      expect(moved).toBeTruthy();
+      moved = pawn.moveTo(6,6);
+      expect(moved).toBeFalsy();
+    });
+
   });
 
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
 
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrow("song is already playing");
+  //
+  //  Paralyzer TEST
+  //
+  describe("Paralyzers", function() {
+    beforeEach(function() {
+      var paralyzer = new Paralyzer(4,4);
     });
+
+    it("should have the correct properties", function() {
+      var paralyzer = new Paralyzer(4,4);
+
+      var currentPos = paralyzer.getPos();
+      expect(currentPos.x).toEqual(4);
+      expect(currentPos.y).toEqual(4);
+      expect(paralyzer.movesDiagonally).toBeFalsy();
+      expect(paralyzer.movementLimit).toEqual(7);
+    });
+
+    it("canMoveTo should give a board with valid movements", function() {
+      var paralyzer = new Paralyzer(4,4);
+
+      var currentPos = paralyzer.getPos();
+      var movePositions = paralyzer.canMoveTo();
+
+      expect(movePositions[currentPos.y][currentPos.x-2]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+2]).toBeTruthy();
+      expect(movePositions[currentPos.y-2][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+2][currentPos.x]).toBeTruthy();
+
+      expect(movePositions[currentPos.y+3][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y-3][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+3]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x-3]).toBeTruthy();
+
+      expect(movePositions[currentPos.y+1][currentPos.x+1]).toBeFalsy();
+      expect(movePositions[currentPos.y-1][currentPos.x-1]).toBeFalsy();
+      expect(movePositions[currentPos.y-1][currentPos.x+1]).toBeFalsy();
+      expect(movePositions[currentPos.y+1][currentPos.x-1]).toBeFalsy();
+
+    });
+
+    it("should be able to moveTo a valid location", function() {
+      var paralyzer = new Paralyzer(4,4);
+
+      var currentPos = paralyzer.getPos();
+      var moved = paralyzer.moveTo(4,3);
+      expect(moved).toBeTruthy();
+      moved = paralyzer.moveTo(6,6);
+      expect(moved).toBeFalsy();
+    });
+
   });
+
+
+
+  //
+  //  Chameleon TEST
+  //
+  describe("Paralyzers", function() {
+    beforeEach(function() {
+      var chameleon = new Chameleon(4,4);
+    });
+
+    it("should have the correct properties", function() {
+      var chameleon = new Chameleon(4,4);
+
+      var currentPos = chameleon.getPos();
+      expect(currentPos.x).toEqual(4);
+      expect(currentPos.y).toEqual(4);
+      expect(chameleon.movesDiagonally).toBeTruthy();
+      expect(chameleon.movementLimit).toEqual(7);
+    });
+
+    it("canMoveTo should give a board with valid movements", function() {
+      var chameleon = new Chameleon(4,4);
+
+      var currentPos = chameleon.getPos();
+      var movePositions = chameleon.canMoveTo();
+      expect(movePositions[currentPos.y][currentPos.x-2]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+2]).toBeTruthy();
+      expect(movePositions[currentPos.y-2][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+2][currentPos.x]).toBeTruthy();
+
+
+      expect(movePositions[currentPos.y+3][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y-3][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+3]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x-3]).toBeTruthy();
+
+      expect(movePositions[currentPos.y+1][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x-1]).toBeTruthy();
+
+      expect(movePositions[currentPos.y+1][currentPos.x+3]).toBeFalsy();
+
+    });
+
+    it("should be able to moveTo a valid location", function() {
+      var chameleon = new Chameleon(4,4);
+
+      var currentPos = chameleon.getPos();
+      var moved = chameleon.moveTo(4,3);
+      expect(moved).toBeTruthy();
+
+      moved = chameleon.moveTo(6,5);
+      expect(moved).toBeTruthy();
+
+      moved = chameleon.moveTo(1,6);
+      expect(moved).toBeFalsy();
+    });
+
+  });
+
+  //
+  //  Retractor TEST
+  //
+  describe("Retractors", function() {
+    beforeEach(function() {
+      var retractor = new Retractor(4,4);
+    });
+
+    it("should have the correct properties", function() {
+      var retractor = new Retractor(4,4);
+
+      var currentPos = retractor.getPos();
+      expect(currentPos.x).toEqual(4);
+      expect(currentPos.y).toEqual(4);
+      expect(retractor.movesDiagonally).toBeTruthy();
+      expect(retractor.movementLimit).toEqual(7);
+    });
+
+    it("canMoveTo should give a board with valid movements", function() {
+      var retractor = new Retractor(4,4);
+
+      var currentPos = retractor.getPos();
+      var movePositions = retractor.canMoveTo();
+      expect(movePositions[currentPos.y][currentPos.x-2]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+2]).toBeTruthy();
+      expect(movePositions[currentPos.y-2][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+2][currentPos.x]).toBeTruthy();
+
+
+      expect(movePositions[currentPos.y+3][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y-3][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+3]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x-3]).toBeTruthy();
+
+      expect(movePositions[currentPos.y+1][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x-1]).toBeTruthy();
+
+      expect(movePositions[currentPos.y+1][currentPos.x+3]).toBeFalsy();
+
+    });
+
+    it("should be able to moveTo a valid location", function() {
+      var retractor = new Retractor(4,4);
+
+      var currentPos = retractor.getPos();
+      var moved = retractor.moveTo(4,3);
+      expect(moved).toBeTruthy();
+
+      moved = retractor.moveTo(6,5);
+      expect(moved).toBeTruthy();
+
+      moved = retractor.moveTo(1,6);
+      expect(moved).toBeFalsy();
+    });
+
+  });
+
+  //
+  //  synchronizer TEST
+  //
+  describe("synchronizers", function() {
+    beforeEach(function() {
+      var synchronizer = new Synchronizer(4,4);
+    });
+
+    it("should have the correct properties", function() {
+      var synchronizer = new Synchronizer(4,4);
+
+      var currentPos = synchronizer.getPos();
+      expect(currentPos.x).toEqual(4);
+      expect(currentPos.y).toEqual(4);
+      expect(synchronizer.movesDiagonally).toBeTruthy();
+      expect(synchronizer.movementLimit).toEqual(7);
+    });
+
+    it("canMoveTo should give a board with valid movements", function() {
+      var synchronizer = new Synchronizer(4,4);
+
+      var currentPos = synchronizer.getPos();
+      var movePositions = synchronizer.canMoveTo();
+      expect(movePositions[currentPos.y][currentPos.x-2]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+2]).toBeTruthy();
+      expect(movePositions[currentPos.y-2][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+2][currentPos.x]).toBeTruthy();
+
+
+      expect(movePositions[currentPos.y+3][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y-3][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+3]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x-3]).toBeTruthy();
+
+      expect(movePositions[currentPos.y+1][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x-1]).toBeTruthy();
+
+      expect(movePositions[currentPos.y+1][currentPos.x+3]).toBeFalsy();
+
+    });
+
+    it("should be able to moveTo a valid location", function() {
+      var synchronizer = new Synchronizer(4,4);
+
+      var currentPos = synchronizer.getPos();
+      var moved = synchronizer.moveTo(4,3);
+      expect(moved).toBeTruthy();
+
+      moved = synchronizer.moveTo(6,5);
+      expect(moved).toBeTruthy();
+
+      moved = synchronizer.moveTo(1,6);
+      expect(moved).toBeFalsy();
+    });
+
+  });
+
+
+  //
+  //  synchronizer TEST
+  //
+  describe("Jumpers", function() {
+    beforeEach(function() {
+      var jumper = new Jumper(4,4);
+    });
+
+    it("should have the correct properties", function() {
+      var jumper = new Jumper(4,4);
+
+      var currentPos = jumper.getPos();
+      expect(currentPos.x).toEqual(4);
+      expect(currentPos.y).toEqual(4);
+      expect(jumper.movesDiagonally).toBeTruthy();
+      expect(jumper.movementLimit).toEqual(7);
+    });
+
+    it("canMoveTo should give a board with valid movements", function() {
+      var jumper = new Jumper(4,4);
+
+      var currentPos = jumper.getPos();
+      var movePositions = jumper.canMoveTo();
+      expect(movePositions[currentPos.y][currentPos.x-2]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+2]).toBeTruthy();
+      expect(movePositions[currentPos.y-2][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+2][currentPos.x]).toBeTruthy();
+
+
+      expect(movePositions[currentPos.y+3][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y-3][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+3]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x-3]).toBeTruthy();
+
+      expect(movePositions[currentPos.y+1][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x-1]).toBeTruthy();
+
+      expect(movePositions[currentPos.y+1][currentPos.x+3]).toBeFalsy();
+
+    });
+
+    it("should be able to moveTo a valid location", function() {
+      var jumper = new Jumper(4,4);
+
+      var currentPos = jumper.getPos();
+      var moved = jumper.moveTo(4,3);
+      expect(moved).toBeTruthy();
+
+      moved = jumper.moveTo(6,5);
+      expect(moved).toBeTruthy();
+
+      moved = jumper.moveTo(1,6);
+      expect(moved).toBeFalsy();
+    });
+
+  });
+
+
+  //
+  //  king TEST
+  //
+  describe("kings", function() {
+    beforeEach(function() {
+      var king = new King(4,4);
+    });
+
+    it("should have the correct properties", function() {
+      var king = new King(4,4);
+
+      var currentPos = king.getPos();
+      expect(currentPos.x).toEqual(4);
+      expect(currentPos.y).toEqual(4);
+      expect(king.movesDiagonally).toBeTruthy();
+      expect(king.movementLimit).toEqual(1);
+    });
+
+    it("canMoveTo should give a board with valid movements", function() {
+      var king = new King(4,4);
+
+      var currentPos = king.getPos();
+      var movePositions = king.canMoveTo();
+      expect(movePositions[currentPos.y][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x]).toBeTruthy();
+
+      expect(movePositions[currentPos.y+1][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x-1]).toBeTruthy();
+      expect(movePositions[currentPos.y-1][currentPos.x+1]).toBeTruthy();
+      expect(movePositions[currentPos.y+1][currentPos.x-1]).toBeTruthy();
+
+      expect(movePositions[currentPos.y][currentPos.x-2]).toBeFalsy();
+      expect(movePositions[currentPos.y][currentPos.x-3]).toBeFalsy();
+      expect(movePositions[currentPos.y][currentPos.x+2]).toBeFalsy();
+      expect(movePositions[currentPos.y][currentPos.x+3]).toBeFalsy();
+      expect(movePositions[currentPos.y-2][currentPos.x]).toBeFalsy();
+      expect(movePositions[currentPos.y-3][currentPos.x]).toBeFalsy();
+      expect(movePositions[currentPos.y+2][currentPos.x]).toBeFalsy();
+      expect(movePositions[currentPos.y+3][currentPos.x]).toBeFalsy();
+      expect(movePositions[currentPos.y+1][currentPos.x+3]).toBeFalsy();
+
+    });
+
+    it("should be able to moveTo a valid location", function() {
+      var king = new King(4,4);
+
+      var currentPos = king.getPos();
+      var moved = king.moveTo(4,3);
+      expect(moved).toBeTruthy();
+
+      moved = king.moveTo(6,5);
+      expect(moved).toBeFalsy();
+
+      moved = king.moveTo(1,6);
+      expect(moved).toBeFalsy();
+    });
+
+  });
+
+  
+
+
 });
